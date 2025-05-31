ccac65917c06ef9b56fb7f3418d94354a3c9f1d3
maxwelrobson <maxps2@live.com>
2025-05-31 15:45:55 -0300

    Alteração na classe CategoriaService

--- demo/src/main/java/com/example/demo/service/CategoriaService.java
+++ demo/src/main/java/com/example/demo/service/CategoriaService.java
@@ -28,18 +28,3 @@ public class CategoriaService {
 	}
 }
 
-import org.springframework.stereotype.Service;
-import com.example.demo.entity.Categoria;
-import com.example.demo.repository.CategoriaRepository;
-	
-	@Service
-	public class CategoriaService {
-	
-		@Autowired
-		private CategoriaRepository repository;
-			
-		
-	public Categoria inserir (Categoria categoria) {
-		return repository.save(categoria);		
-	}
-	}
