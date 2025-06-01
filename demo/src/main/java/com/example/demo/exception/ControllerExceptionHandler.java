90e9d5cbf9d7568657fb571c355f1569755f40cc
matheusruella <matheusruella7@gmail.com>
2025-06-01 20:43:22 -0300

    Exception atualizadas com @ControllerAdvice

--- demo/src/main/java/com/example/demo/exception/ControllerExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/ControllerExceptionHandler.java
@@ -5,17 +5,16 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.web.servlet.function.ServerResponse.badRequest;
-
+@ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
