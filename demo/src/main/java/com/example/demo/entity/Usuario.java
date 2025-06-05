4dea592dd60dfba69a79b4a7d9cf054e8454ce33
matheusruella <matheusruella7@gmail.com>
2025-06-05 00:53:00 -0300

    bug fix

--- demo/src/main/java/com/example/demo/entity/Usuario.java
+++ demo/src/main/java/com/example/demo/entity/Usuario.java
@@ -1,111 +1,104 @@
 package com.example.demo.entity;
 
-import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-
+import jakarta.persistence.OneToOne;
 
 @Entity
-public class Usuario implements UserDetails { // Implementa UserDetails para integrar com Spring Security
+
+public class Usuario {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) // Gera ID automaticamente no banco
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private String numero;
+    private String complemento;
     private String email;
-    private String senha; 
+	private String cep;
 
-    // um usuário pode usar vários perfis, relaciona usuario com perfil
-    @OneToMany(mappedBy = "id.usuario")
-    private Set<UsuarioPerfil> usuarioPerfis = new HashSet<>();
+	@OneToMany(mappedBy = "usuario")
+	private List<Endereco> enderecos;
+	
+	@OneToOne
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
 
-    @ManyToOne
-    @JoinColumn(name = "id_endereco") // Define a chave estrangeira
-    private Endereco endereco;
+	@OneToMany(mappedBy = "id.usuario")
+	private Set<UsuarioPerfil> usuarioPerfis = new HashSet<>();
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	
+
+	}
     
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getSenha() {
-        return senha;
-    }
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-    public Endereco getEndereco() {
-        return endereco;
-    }
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-    public Set<UsuarioPerfil> getUsuarioPerfis() {
-        return usuarioPerfis;
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + "]";
-    }
-
-    // Retorna as permissões do usuário com base nos perfis que ele possui
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return usuarioPerfis.stream()
-            .map(up -> new SimpleGrantedAuthority(up.getId().getPerfil().getNome().toUpperCase())) // Agora acessa via `getId()`
-            .collect(Collectors.toSet());
-    }
-
-    // indica se a conta está ativa e válida
-    @Override
-    public String getPassword() { // Retorna a senha do usuário
-        return senha;
-    }
-    @Override
-    public String getUsername() { // Define o login como o email
-        return email;
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return true; // Indica que a conta não está expirada
-    }
-    @Override
-    public boolean isAccountNonLocked() {
-        return true; // Indica que a conta não está bloqueada
-    }
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true; // Indica que as credenciais não expiraram
-    }
-    @Override
-    public boolean isEnabled() {
-        return true; // Indica que a conta está ativa
-    }
-
 }
